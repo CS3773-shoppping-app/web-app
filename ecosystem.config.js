08b43b46def95953d9079593e8eead041c4931b2
EC2 Default User <ec2-user@ip-172-31-26-117.us-west-2.compute.internal>
2024-06-25 02:01:14 +0000

    updated index.js, ci/cd action added, ecosystemconfig added.

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: "shopping-app",  // A name for your application
+    script: "index.js",  // The script file to launch the app
+    instances: "max",  // This can be set to the number of CPU cores to use, or "max" to use all available
+    autorestart: true,  // Automatically restart app if it crashes
+    watch: false,  // Enable/disable watching file changes for automatic restart
+    max_memory_restart: '1G',  // Optional: Restart app if it reaches a memory limit
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+};
