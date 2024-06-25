e9120b07efcbc05deca7432b0c8a71831fa0e413
Matthew Garza <matt.garza34@gmail.com>
2024-06-24 21:16:41 -0500

    testing ci/cd. added red color

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end('<html><body><h1>Hello, World!</h1></body></html>');
+ res.end('<html><body><h1 style="color:red;">Hello, World!</h1></body></html>');
 })
 
 server.listen(port, hostname);
