0f2575338daf5c785ea6279dae53b3ee5d8c64c6
jchav2 <jonathan.chavez3896@gmail.com>
2024-06-27 10:44:08 -0500

    configured .env for deployment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
