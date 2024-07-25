b66f19f67d8b5eefd9fda1462c9a9c53d90ec49e
Matthew Garza <matt.garza34@gmail.com>
2024-07-24 23:20:58 -0500

    installed formidable, updated upload route to next 14 method.

--- next-shopping-app/src/app/Api/products/upload/route.js
+++ next-shopping-app/src/app/Api/products/upload/route.js
@@ -4,29 +4,24 @@ import { v4 as uuidv4 } from 'uuid';
 import mysql from 'mysql2/promise';
 import { DbConnect } from '../route';
 
-const parseForm = (req) => {
-  return new Promise((resolve, reject) => {
+const parseForm = (req) =>
+  new Promise((resolve, reject) => {
     const form = new IncomingForm();
     form.parse(req, (err, fields, files) => {
       if (err) return reject(err);
       resolve({ fields, files });
     });
   });
-};
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async (req, res) => {
-  if (req.method === 'POST') {
+export default async function POST(req){
     try {
-      const { fields, files } = await parseForm(req);
+        const { fields, files } = await parseForm(req);
+        const { name, description, price, stockAmount } = fields;
+        const image = files.image;
 
-      const { name, description, price, stockAmount } = fields;
-      const image = files.image;
+        if (!image) {
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 });
+          }
 
       const imagePath = `./public/uploads/${uuidv4()}-${image.originalFilename}`;
       const imagePublicPath = `/uploads/${uuidv4()}-${image.originalFilename}`;
@@ -34,7 +29,7 @@ export default async (req, res) => {
       fs.renameSync(image.filepath, imagePath);
       const connection = await DbConnect();
       const [itemResult] = await connection.execute(
-        'INSERT INTO Items (name, description, price, quantity_available) VALUES (?, ?, ?, ?)',
+        'INSERT INTO Items (name, description, price, quantity_available) VALUES (?, ?, ?, ?);',
         [name, description, price, stockAmount]
       );
 
@@ -47,12 +42,11 @@ export default async (req, res) => {
 
       await connection.end();
 
-      res.status(200).json({ message: 'Product uploaded successfully' });
+      return NextResponse.json({ message: 'Product uploaded successfully' });
     } catch (error) {
-      console.error('Error uploading product:', error);
-      res.status(500).json({ message: 'Failed to upload product' });
+        console.error('Error uploading product:', error);
+        return NextResponse.json({ message: 'Failed to upload product' }, { status: 500 });
     }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
   }
-};
+    
+
